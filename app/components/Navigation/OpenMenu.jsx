fb4fa692be52a835e8c68aba0f9c86629780b7e4
Eduardo <ed98lopez@gmail.com>
2023-11-12 00:29:22 -0800

    added a fully funcitonal nav. need to go back and revise and clean code.

--- app/components/Navigation/OpenMenu.jsx
+++ app/components/Navigation/OpenMenu.jsx
@@ -3,16 +3,43 @@ import React from "react";
 import styles from "./Navigation.module.css";
 import { useOpen } from "./NavProvider";
 import { RiCloseFill } from "react-icons/ri";
+import Link from "next/link";
+import Image from "next/image";
+
+const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "Beer", path: "/beer" },
+  { name: "Story", path: "/story" },
+  { name: "Events", path: "/events" },
+  { name: "Blog", path: "/blog" },
+];
 
 const OpenMenu = () => {
   const { isOpen, dispatch } = useOpen();
 
   return (
-    <div id="menu"
-      className={`${styles.menu} bg-2 ${isOpen==true && styles.openMenu}`}
-    >
-      <div onClick={dispatch} className="text-5xl">
+    <div className={`${styles.menu} bg-2 ${isOpen == true && styles.openMenu}`}>
+      <div onClick={dispatch} className={styles.close}>
         <RiCloseFill className="text-white" />
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image src="/images/logo-full.svg" width={50} height={50} alt="logo" />
+      </div>
+
+      <div>
+        <div className={styles.menuList}>
+          {navLinks.map((link, i) => {
+            return (
+              <Link key={i} href={link.path} onClick={dispatch}>
+                {link.name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className={styles.button}>
+        <button className="btn">Shop</button>
       </div>
     </div>
   );
