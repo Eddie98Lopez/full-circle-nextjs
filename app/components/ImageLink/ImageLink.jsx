0da9bbb189aff88172c4a8ecaa6002f4e585e270
Eduardo <ed98lopez@gmail.com>
2023-11-29 19:22:26 -0800

    animated link

--- app/components/ImageLink/ImageLink.jsx
+++ app/components/ImageLink/ImageLink.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
+import styles from './ImageLink.module.css'
+import useOnScreen from "@/app/utils/useOnScreen";
 
 const ImageLink = ({link,...props}) => {
-    const {background, href } = link
+  const linkRef= useRef()
+  const visible = useOnScreen(linkRef)
+  
+ 
+    const {background, display, href } = link
   return (
-    <div>ImageLink</div>
+    <li
+    ref={linkRef}
+      key={Math.random()}
+      className={`${styles.item} display-2 ${visible && 'animate__fadeIn'}`}
+      style={{ backgroundImage: `url(${href})` }}
+    >
+      <div className={styles.bgdiv}>
+        
+      </div>
+      <a>{display}</a>
+    </li>
   )
 }
 
--- app/components/ImageLink/ImageLinkList.jsx
+++ app/components/ImageLink/ImageLinkList.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import styles from "./ImageLink.module.css";
-import { url } from "inspector";
+
+import ImageLink from "./ImageLink";
 
 const links = [
   { display: "find our beer", href: "/images/find-beer.webp" },
   { display: "black owned", href: "/images/black-owned.webp" },
   { display: "#goodvibesonly", href: "/images/boop.webp" },
-  {display:"events",href:'/images/events.webp'},
+  { display: "events", href: "/images/events.webp" },
 ];
 
 const ImageLinkList = () => {
   return (
     <ul className={styles.list}>
       {links.map((item) => (
-        <li
-          key={Math.random()}
-          className={`${styles.item} display-2`}
-          style={{ backgroundImage: `url(${item.href})` }}
-        >
-          <div className={styles.bgdiv}>
-            
-          </div>
-          <a>{item.display}</a>
-        </li>
+        <ImageLink key={Math.random()} link={item} />
       ))}
     </ul>
   );
