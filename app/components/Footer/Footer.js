aba5669fe93174e18005eb710707a03cccbc2c2c
Eduardo <ed98lopez@gmail.com>
2023-12-05 00:15:45 -0800

    made the social list component alignment more flexible with a prop

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -7,13 +7,19 @@ import socialDummyData from "../SocialComponent/dummyData";
 const Footer = (props) => {
   return (
     <footer className={`bg-2 ${styles.footer}`}>
+      <div className={styles.sub}>
       <SubscribeForm />
-      <div className={styles.divider}></div>
-      <div>links</div>
-      <div>
-        <h3 className="display-1">Follow us</h3>
-        <SocialList array={socialDummyData} theme="light" />
       </div>
+      
+      <div className={styles.divider}></div>
+      <div className={styles.links}>links</div>
+      <div className={styles.social}>
+        <h3 className="display-1 text-2xl">Follow us</h3>
+        <SocialList array={socialDummyData} theme="light" align='left' />
+      </div>
+      <div className={styles.order}></div>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.hours}>Hours</div>
     </footer>
   );
 };
---
+++ app/components/Navigation/links.js
@@ -0,0 +1,11 @@
+const links = [
+    { name: "Home", path: "/" },
+    { name: "Beer", path: "/beer" },
+    { name: "Story", path: "/story" },
+    { name: "Events", path: "/events" },
+    { name: "Blog", path: "/blog" },
+    { name: "Contact", path: "/contact" },
+    { name: "careers", path: "/careers" },
+  ];
+
+export default links
