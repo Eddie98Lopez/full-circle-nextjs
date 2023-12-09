c7ee38f068d99f580df1b746f5e11631e460e9d4
Eduardo <ed98lopez@gmail.com>
2023-12-09 13:16:37 -0800

    fixed the nav styling. Had to pay extra special attention to parent divs and styles inheritance.

--- app/components/Navigation/NavLogoSection/LogoSocial.js
+++ app/components/Navigation/NavLogoSection/LogoSocial.js
@@ -1,12 +1,12 @@
 import React from "react";
-import styles from "./NavLinkList/NavList.module.css";
+import styles from "./LogoSocial.module.css";
 import Image from "next/image";
-import socialDummyData from "../SocialComponent/dummyData";
-import SocialList from "../SocialComponent/SocialList";
+import socialDummyData from "../../SocialComponent/dummyData";
+import SocialList from "../../SocialComponent/SocialList";
 
 const LogoSocial = () => {
   return (
-    <div className="flex-col place-content-center w-full center">
+    <div className={ `${styles.wrapper}`}>
       <div className={styles.imageWrapper}>
         <Image
           src={"/images/opaque-full-logo.svg"}
