7c1fa131aab39475842db3a5b83c475d66963839
Eduardo <ed98lopez@gmail.com>
2024-01-21 21:55:35 -0800

    added dummy blog data

--- app/components/Blog/BlogSliderThumb/BlogSliderThumb.js
+++ app/components/Blog/BlogSliderThumb/BlogSliderThumb.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./BlogSliderThumb.module.css";
+import Image from "next/image";
 
-const BlogSliderThumb = () => {
+const BlogSliderThumb = ({blog,...props}) => {
+    const{cover_photo, title, id} = blog
   return (
     <div className={`${styles.thumb}`}>
       <li>
-        <div className={`${styles.img}`}>img</div>
+        <div className={`${styles.img}`}>
+            <Image alt='cover-image' src={cover_photo} fill/>
+        </div>
         <div className={`${styles.gradient}`}/>
-        <h3 className={`${styles.title} display-1 text-white`}>Blog Post  Title</h3>
+        <h3 className={`${styles.title} display-1 text-white`}>{title}</h3>
       </li>
     </div>
   );
---
+++ app/components/Blog/dummyData.js
@@ -0,0 +1,34 @@
+const blogDummyData = [
+    {
+        id:1,
+        title:'What is An Adjunct In Brewing',
+        cover_photo: '/images/what-is-brewing.webp',
+
+    },
+    {
+        id:2,
+        title:'Brewery Collaborations: Working with Humble Sea Brewing Co.',
+        cover_photo: '/images/collaborations.webp',
+        
+    },
+    {
+        id:3,
+        title:'We Head to Portland for Internationally Celebrated Craft Beer Event Brews for New Avenues!',
+        cover_photo: '/images/portland.gif',
+        
+    },
+    {
+        id:4,
+        title:'How to be Happy: Discover the Joy of Beer & Music',
+        cover_photo: '/images/discover-joy.webp',
+        
+    },
+    {
+        id:5,
+        title:'Why Beer Cans Are Better',
+        cover_photo: '/images/beer-cans-better.webp',
+        
+    }
+]
+
+export default blogDummyData
